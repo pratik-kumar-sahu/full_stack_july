036e1469ee9850879f94aab17070fe29e91393b1
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-07-11 14:50:17 +0530

    Added toast for helper messages

--- src/components/ModalForm.js
+++ src/components/ModalForm.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./styles.scss";
 import Tag from "./Tag";
+
+toast.configure();
 
 function ModalForm({ setModal }) {
   const [formData, setFormData] = useState({
@@ -21,10 +25,50 @@ function ModalForm({ setModal }) {
     });
   };
 
+  const validate = () => {
+    const { title, description, date, tags, color, status } = formData;
+
+    if (title.trim().length < 2) {
+      toast.error("Title should be min. 2 characters", { autoClose: 3000 });
+      return false;
+    } else if (description.trim().length < 10) {
+      toast.error("Description should be min. 10 characters", {
+        autoClose: 3000,
+      });
+      return false;
+    } else if (!date.trim()) {
+      toast.error("Due Date is required", {
+        autoClose: 3000,
+      });
+      return false;
+    } else if (!status.trim()) {
+      toast.error("Select status", {
+        autoClose: 3000,
+      });
+      return false;
+    } else if (tags.length === 0) {
+      toast.error("Select atleast one tag", {
+        autoClose: 3000,
+      });
+      return false;
+    } else if (color.trim().length < 1) {
+      toast.error("Select a color", {
+        autoClose: 3000,
+      });
+      return false;
+    }
+
+    return true;
+  };
+
   const submitResponse = (e) => {
     e.preventDefault();
     console.log(formData);
-    setModal(false);
+
+    if (validate()) {
+      setModal(false);
+      toast.success("Successful", { autoClose: 3000 });
+    }
   };
 
   return (
@@ -38,6 +82,7 @@ function ModalForm({ setModal }) {
           value={formData.title}
           onChange={onChangehandler}
         />
+
         <input
           className="modal__container-input"
           type="text"
@@ -46,6 +91,7 @@ function ModalForm({ setModal }) {
           value={formData.description}
           onChange={onChangehandler}
         />
+
         <input
           className="modal__container-input"
           type="date"
@@ -54,39 +100,50 @@ function ModalForm({ setModal }) {
           value={formData.date}
           onChange={onChangehandler}
         />
-        <div className="modal__container-tags">
-          {["Daily", "Work", "Important"].map((tag, i) => (
-            <Tag
-              key={i}
-              id={i}
-              tag={tag}
-              formData={formData}
-              setFormData={setFormData}
-            />
-          ))}
-          {/* <button>Add Tag</button> */}
-        </div>
-        <input
-          type="color"
-          name="color"
-          value={formData.color}
-          onChange={onChangehandler}
-        />
+
         <div className="modal__container-status">
           <select
+            className="modal__container-input input2"
             name="status"
             value={formData.status}
             onChange={onChangehandler}
           >
-            <option value="" defaultValue>
-              ---
+            <option value="" defaultValue disabled>
+              Status
             </option>
             <option value="backlog">Backlog</option>
             <option value="in-progress">In Progress</option>
             <option value="done">Done</option>
           </select>
         </div>
-        <button type="submit">Submit</button>
+
+        <div>
+          <div className="modal__container-tags">
+            <span style={{ marginRight: ".5rem" }}>📌</span>
+            {["Daily", "Work", "Important"].map((tag, i) => (
+              <Tag
+                key={i}
+                id={i}
+                tag={tag}
+                formData={formData}
+                setFormData={setFormData}
+              />
+            ))}
+            {/* <button>Add Tag</button> */}
+          </div>
+
+          <input
+            className="modal__container-color"
+            type="color"
+            name="color"
+            value={formData.color ? formData.color : "#ff0000"}
+            onChange={onChangehandler}
+          />
+
+          <button className="modal__container-submit" type="submit">
+            Submit
+          </button>
+        </div>
       </form>
     </div>
   );
